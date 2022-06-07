fb1b45d881b1d37d134ac17f378faf3c303454ce
AhmedNader <ahmed.nader1994@gmail.com>
2022-06-07 17:11:07 +0200

    first commit

---
+++ app/src/main/java/com/udacity/shoestore/ui/onboarding/OnboardingFragment.kt
@@ -0,0 +1,36 @@
+package com.udacity.shoestore.ui.onboarding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.databinding.FragmentOnboardingBinding
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+
+class OnboardingFragment : Fragment() {
+
+    private var _binding: FragmentOnboardingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root.rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/udacity/shoestore/ui/welcome/WelcomeFragment.kt
@@ -0,0 +1,37 @@
+package com.udacity.shoestore.ui.welcome
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root.rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.start.setOnClickListener {
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToLoginFragment())
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
